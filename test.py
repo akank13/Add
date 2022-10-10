304fec4f294665d596ea1afd1544b3132080573e
AKANKSHA MISHRA <akki97531@gmail.com>
2022-10-11 01:03:51 +0530

    add code

---
+++ test.py
@@ -0,0 +1,18 @@
+
+
+print("hello world")
+#Add 2 no.s by User Input
+x= input("please enter your 1st number:  ")
+
+y= input("please enter your 2nd number:  ")
+
+sum = int(x) + int(y)
+
+print("The sum is :  ", sum)
+
+
+#Multiply by 2 by User Input
+a= input("please enter your number: ")
+multiply = int (a)*2 
+
+print("The product = ", multiply)
