0a737cd2d4644c4c1c2e459f566e25d382ee8d89
AKANKSHA MISHRA <akki97531@gmail.com>
2022-10-11 16:59:46 +0530

    Area

---
+++ maths.py
@@ -0,0 +1,11 @@
+#Area of the Triangle
+
+from numpy import multiply
+
+
+b= float(input("enter the breadth of the triangle: "))
+h= float(input("enter the height of the triangle: "))
+
+multiply = (b * h)*0.5
+
+print("Area of Triangle :  ", multiply)
